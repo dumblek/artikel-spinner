539bd518fd8a0929da84788a752b022667bc4d9b
ayyoub maulana <ayyoub@ayyoubs-MacBook-Air.local>
2021-06-13 16:51:29 +0700

    adding files

--- synonim_indonesia.py
+++ synonim_indonesia.py
@@ -7,9 +7,13 @@ Created on Sun Jun 13 16:21:54 2021
 """
 
 import re, pandas as pd
+import pandasql as ps
 
 with open('/Users/ayyoub/de_project/Daftar-Antonim-Tesaurus-Bahasa-Indonesia/tesaurus-id.txt', 'r') as f:
     lines = f.readlines()
     
-lines_clear = [[re.sub(r'\d\,\s',',', y) for y in re.sub(r'^\-.*|^\d|(\;|)\n$|\d+\s+', '', re.sub(r'\s(n|v|p|a)\s', ', ', x.replace('- ',''))).split('; ') if y] for x in lines]
+lines_clear = [[re.sub(r'\d\,\s',',', y) for y in re.sub(r'^\-.*|^\d|(\;|)\n$|\d+\s+', '', re.sub(r'\s(n|v|p|a|pron)\s', ', ', x.replace('- ',''))).split('; ') if y] for x in lines]
 df_sin = pd.DataFrame(lines_clear)
+df_sin = df_sin.rename(columns={0:'word'})[['word']]
+
+ps.sqldf("select word from df_sin where word like 'bengkok,%' or word like '%,bengkok' or word like '%, bengkok' or word like '%,bengkok,%' or word like '%, bengkok,%'")
